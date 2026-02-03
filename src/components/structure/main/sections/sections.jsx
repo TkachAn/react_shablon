2c432ef4920ec9a8c163b0c745844b707d37b257
OksanaTkachevska <61586895+TkachAn@users.noreply.github.com>
2026-02-03 08:46:49 +0200

    set03022026

--- src/components/structure/main/sections/sections.jsx
+++ src/components/structure/main/sections/sections.jsx
@@ -1,28 +1,48 @@
 // src/components/structure/Section/Section.jsx
+import { Container } from "../../container/container";
 import s from "./s.module.css";
+
+export function Header({ children }) {
+  return (
+    <header className={s.header}>
+      <Container>{children}</Container>
+    </header>
+  );
+}
+
+export function Main({ children }) {
+  return <main className={s.main}>{children}</main>;
+}
 
 export function Hero({ children }) {
   return <section className={s.hero}>{children}</section>;
 }
 
-export function Section({ children, title, id }) {
+export function Section({ children, title, id, className }) {
   return (
-    <section id={id} className={s.section}>
+    <section id={id} className={`${s.section} ${className || ""}`}>
       <h2 className={s.section_title}>{title}</h2>
       {children}
     </section>
   );
 }
-export function Flex({ children, id }) {
+export function Grid({ children, id, className }) {
   return (
-    <section id={id} className={s.section_flex}>
+    <div id={id} className={`${s.grid} ${className || ""}`}>
       {children}
-    </section>
+    </div>
   );
 }
-export function Article({ children, title, id }) {
+export function Flex({ children, id, className }) {
   return (
-    <article id={id} className={s.article}>
+    <div id={id} className={`${s.flex} ${className || ""}`}>
+      {children}
+    </div>
+  );
+}
+export function Article({ children, title, id, className }) {
+  return (
+    <article id={id} className={`${s.article} ${className || ""}`}>
       <h3 className={s.article_title}>{title}</h3>
       {children}
     </article>
@@ -43,5 +63,12 @@ export function AsideRight({ children, title, id, className }) {
       <h3 className={s.aside_title}>{title}</h3>
       {children}
     </aside>
+  );
+}
+export function Footer({ children }) {
+  return (
+    <footer className={s.footer}>
+      <Container>{children}</Container>
+    </footer>
   );
 }
